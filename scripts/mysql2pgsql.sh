92143c15216f663c93129edd2e7b51884d157536
Hauke Zuehl <hzuehl@phone-talk.de>
2012-11-19 16:00:39 +0100

    First lines of code. To be continued...

---
+++ scripts/mysql2pgsql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is for converting friendica's database from MySQL to
+# PostgreSQL.
+#
+# USE AT YOUR OWN RISK!
+
+MYSQL="$1"
+PGSQL="$2"
+
+echo "Loading MySQL table in $1..."
+cat $MYSQL | sed \
+		-e 's/`//g' \
+		-e 's/IF NOT EXISTS/ /g' \
+		-e 's/ENGINE=MyISAM *DEFAULT CHARSET=utf8//g' \
+		-e 's/char(255)/varchar(255)/g' \
+		-e 's/\(.*\) [int|INT].* \(NOT NULL\) AUTO_INCREMENT/\1 serial \2/g'

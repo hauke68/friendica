a901d02a7a56e47efc203162638e71b21a06ed4a
Hauke Zuehl <hzuehl@phone-talk.de>
2012-11-20 08:32:13 +0100

    Some more type changes added.

--- scripts/mysql2pgsql.sh
+++ scripts/mysql2pgsql.sh
@@ -6,14 +6,23 @@
 # USE AT YOUR OWN RISK!
 
 MYSQL="$1"
-PGSQL="$2"
+# PGSQL="$2"
 
-echo "Loading MySQL table in $1..."
 cat $MYSQL | sed \
 		-e 's/`//g' \
-		-e 's/IF NOT EXISTS/ /g' \
+		-e 's/ COMMENT .*;$//g' \
+		-e 's/ UNSIGNED//g' \
+		-e 's/ unsigned//g' \
+		-e 's/ tinyinteger/ INTEGER/g' \
+		-e 's/ tinyint(.*)/ INTEGER/g' \
+		-e 's/ TINYINT(.*)/ INTEGER/g' \
+		-e 's/ bigint(.*)/ INTEGER/g' \
+		-e 's/ mediumtext/ TEXT/g' \
+		-e 's/ IF NOT EXISTS/ /g' \
 		-e 's/ENGINE=MyISAM *DEFAULT CHARSET=utf8//g' \
-		-e 's/char(255)/varchar(255)/g' \
-		-e 's/int(.*)/INTEGER/g' \
-		-e 's/INT(.*)/INTEGER/g' \
-		-e 's/\(.*\) INTEGER.* \(NOT NULL\) AUTO_INCREMENT/\1 serial \2/g'
+		-e 's/ char(255)/ VARCHAR(255)/g' \
+		-e 's/ int(.*)/ INTEGER/g' \
+		-e 's/ INT(.*)/ INTEGER/g' \
+		-e 's/\(.*\) INTEGER.* \(NOT NULL\) AUTO_INCREMENT/\1 SERIAL \2/g' \
+		-e 's/\(.*\) INT \(NOT NULL\) AUTO_INCREMENT/\1 SERIAL \2/g' \
+		-e 's/ datetime/ TIMESTAMP/g' \
